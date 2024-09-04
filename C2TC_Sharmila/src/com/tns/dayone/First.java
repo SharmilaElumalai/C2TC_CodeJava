e598ce92fb7bee1c59f81f9ae2f621b3823e1318
Sharmi <Sharmi@ItsSharmi>
2024-09-04 11:54:47 +0530

    This is my first commit

---
+++ C2TC_Sharmila/src/com/tns/dayone/First.java
@@ -0,0 +1,29 @@
+package com.tns.dayone;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;  
+
+		int b=6;  
+
+		
+
+		//Arithmetic Operator
+
+		System.out.println("Arithmetic Operator");
+
+		System.out.println(a+b);  
+
+		System.out.println(a-b); 
+
+		System.out.println(a*b); 
+
+		System.out.println(a/b);
+
+		System.out.println(a%b);
+
+	}
+
+}
